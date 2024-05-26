1ee9a5ca77a9dcfd0ce6aeeec1de68cec688c468
Srinivasa Narasimhan <snarasim123@gmail.com>
2024-05-25 22:30:18 -0700

    basic fedora install

--- variables.sh
+++ variables.sh
@@ -1,11 +1,13 @@
-# #shared vars
-export distro_name=ubuntu-ansible2
+# distro specific vars
+# export distro_type=ubuntu
+export distro_type=fedora
+# export distro_name=ubuntu2004-ansible3
+export distro_name=fedora_3704-ansible2
+# export ps_distro_source=d:\soft\wsl\ubuntu2004.tar
+export ps_distro_source=d:\soft\wsl\fedora_3704.tar
 
-#powershell vars
-export ps_distro_source=d:\soft\wsl\ubuntu2004.tar
+
+#common vars
 export ps_install_dir=C:\Users\snara\wsl
-
-#bash only vars
 export default_user=snarasim
-export light_install=0
-export basic_install=0
+
