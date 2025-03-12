86fc6b8075a2fb8465dc24a2ceacf33e434442b4
Srinivasa Narasimhan <snarasim123@gmail.com>
2025-03-11 22:28:32 -0700

    alpine python compile specific version

--- prep-install.sh
+++ prep-install.sh
@@ -14,8 +14,34 @@ upgrade() {
         apk add sudo
         sudo apk update
         sudo apk add bash              
-        sudo apk add lsb-release          
+        sudo apk add lsb-release     
+
         sudo apk add --no-cache openssh
+        # sudo apk del --no-cache ansible 
+        sudo apk del --no-cache py3-passlib 
+        sudo apk del --no-cache python3 
+        sudo apk del --no-cache python3-dev 
+        sudo apk del --no-cache py3-pip
+
+        PYTHON_VERSION="3.8.9"
+        PYTHON_MAJOR_VERSION="3.8"
+        sudo apk add --no-cache build-base wget libffi-dev  libbz2 libffi zlib zlib-dev bzip2-dev readline-dev ncurses-dev xz-dev 
+        #tk-dev  
+        sudo apk add  ca-certificates 
+        
+        cd /tmp
+        wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
+        tar -xvzf Python-${PYTHON_VERSION}.tgz
+        cd Python-${PYTHON_VERSION}
+        ./configure --prefix=/usr/local
+        make
+        sudo make install
+
+        sudo ln -sf "/usr/local/bin/python${PYTHON_MAJOR_VERSION}" /usr/bin/python3
+        sudo ln -sf "/usr/local/bin/python${PYTHON_MAJOR_VERSION}" /usr/bin/python
+        sudo ln -sf /usr/local/bin/pip3 /usr/bin/pip3
+
+        read -p "*** prep-install.sh ansible install , Press to continue.. " -n 1 -r
         sudo apk add py3-passlib
         sudo apk add ansible
         ansible-galaxy collection install community.general
